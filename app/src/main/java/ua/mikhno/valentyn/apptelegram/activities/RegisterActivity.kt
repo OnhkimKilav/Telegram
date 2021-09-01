bc1052ab0e91f600e4ee0860538cfa89d196bc0c
Valentyn Mikhno <kilavvalik24@gmail.com>
2021-09-01 13:07:08 +0300

    added RegisterActivity.kt and activity_register.xml, this space is used for contain fragments with registration

---
+++ app/src/main/java/ua/mikhno/valentyn/apptelegram/activities/RegisterActivity.kt
@@ -0,0 +1,30 @@
+package ua.mikhno.valentyn.apptelegram.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import ua.mikhno.valentyn.apptelegram.R
+import ua.mikhno.valentyn.apptelegram.databinding.ActivityRegisterBinding
+import ua.mikhno.valentyn.apptelegram.ui.fragments.EnterPhoneNumberFragment
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityRegisterBinding
+    private lateinit var mToolBar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mToolBar = mBinding.registerToolBar
+        setSupportActionBar(mToolBar)
+        title = getString(R.string.register_title_your_phone)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.registerDataContainer, EnterPhoneNumberFragment())
+            .commit()
+    }
+}
