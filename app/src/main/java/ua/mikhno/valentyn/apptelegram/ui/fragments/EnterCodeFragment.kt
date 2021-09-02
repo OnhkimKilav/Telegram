a4dda337d36a8d905c25f842dcafba57541e649c
Valentyn Mikhno <kilavvalik24@gmail.com>
2021-09-02 09:38:34 +0300

    Created AppTextWatcher.kt where we implemented interface TextWatcher and refactored code in EnterCodeFragment.kt class

--- app/src/main/java/ua/mikhno/valentyn/apptelegram/ui/fragments/EnterCodeFragment.kt
+++ app/src/main/java/ua/mikhno/valentyn/apptelegram/ui/fragments/EnterCodeFragment.kt
@@ -6,24 +6,16 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_enter_code.*
 import ua.mikhno.valentyn.apptelegram.R
+import ua.mikhno.valentyn.apptelegram.utils.AppTextWatcher
 
 class EnterCodeFragment : Fragment(R.layout.fragment_enter_code) {
     override fun onStart() {
         super.onStart()
-        register_input_code.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                val string = register_input_code.text.toString()
-                if (string.length == 5){
-                    verificationCode()
-                }
+        register_input_code.addTextChangedListener(AppTextWatcher {
+            val string = register_input_code.text.toString()
+            if (string.length == 5) {
+                verificationCode()
             }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-
         })
     }
 
